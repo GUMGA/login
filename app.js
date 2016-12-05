ab3033c89b9a27352b4df1e80811b4e264019b23
Mateus Miranda <info.mateusmiranda@gmail.com>
2016-12-05 17:05:48 -0200

    login with social

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 angular
   .module('app',['gumga.login'])
   .controller('LoginController', ['$scope',function($scope) {
+
     $scope.loginGumga = function(login) {
       console.log(login)
     }
+
+    $scope.login = function(dados){
+       console.log('jesusssssss', dados)
+    }
+
   }])
