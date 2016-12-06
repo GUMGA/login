75e819d2d35c522b1c7e7fddeebe4c44cfdd106b
Mateus Miranda <info.mateusmiranda@gmail.com>
2016-12-06 09:08:15 -0200

    commit

--- src/components/google/angular-google-plus.js
+++ src/components/google/angular-google-plus.js
@@ -105,7 +105,6 @@ angular.module('googleplus', []).
       NgGooglePlus.prototype.login =  function () {
         deferred  = $q.defer();
 
-        console.log('agr', options.scopes)
         var authOptions = {
           client_id: options.clientId,
           scope: options.scopes,
