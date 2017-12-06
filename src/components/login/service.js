da0b03a25521c24a8d7cdf3f76890d14fdb5a0b9
Mateus Miranda <info.mateusmiranda@gmail.com>
2017-12-05 22:13:24 -0200

    merge

--- src/components/login/service.js
+++ src/components/login/service.js
@@ -11,7 +11,7 @@ export default function GumgaLoginService($http, $window){
     
     Service.changeOrganization = (token, organization) => {
       return $http.get(Service.configuration['appURL']
-                  .concat('/public/token/changeorganization/'
+                  .concat('/public/token/changeorganizationandtoken/'
                   .concat(token)
                   .concat('/')
                   .concat(organization.id)));
