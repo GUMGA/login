21d2eaae93726abf0bdd49b288951b6ad8abe51f
Mateus Miranda <info.mateusmiranda@gmail.com>
2018-01-29 22:06:33 -0200

    merge

--- src/components/register/register.js
+++ src/components/register/register.js
@@ -9,21 +9,44 @@ let RegisterComponent = {
 
         ctrl.config = {
             titleStepOne: angular.element('gl-login').attr('register-title-step-one') || 'Ebaaa!!!',
-            subTitleStepOne : angular.element('gl-login').attr('register-subtitle-step-one') || 'Estamos muitos felizes por você topar o desafio ;)',
+            subTitleStepOne: angular.element('gl-login').attr('register-subtitle-step-one') || 'Estamos muitos felizes por você topar o desafio ;)',
             textBtnStepOne: angular.element('gl-login').attr('register-text-btn-step-one') || 'Bora lá se registrar',
 
             titleStepTwo: angular.element('gl-login').attr('register-title-step-two') || 'agora você é quase parte da familia',
-            subTitleStepTwo : angular.element('gl-login').attr('register-subtitle-step-two') || 'Seu ambiente virtual na Kigi vai ser incrível',
+            subTitleStepTwo: angular.element('gl-login').attr('register-subtitle-step-two') || 'Seu ambiente virtual na Kigi vai ser incrível',
             textBtnStepTwo: angular.element('gl-login').attr('register-text-btn-step-two') || 'Continuar',
 
-            logo : angular.element('gl-login').attr('register-logo')
+            logo: angular.element('gl-login').attr('register-logo')
         }
 
         ctrl.$onInit = () => {
-            ctrl.step = 5;
+            ctrl.step = 1;
             ctrl.user = {
                 email: params['email']
             }
+            ctrl.initAutoComplete();
+        }
+
+        ctrl.initAutoComplete = () => {
+            ctrl.user.address = {};
+            var defaultBounds = new google.maps.LatLngBounds(
+                new google.maps.LatLng(-33.8902, 151.1759),
+                new google.maps.LatLng(-33.8474, 151.2631));
+
+            var input = document.getElementById('cepInput');
+            let autocomplete = new google.maps.places.Autocomplete(input, {});
+            autocomplete.addListener('place_changed', () => {
+                let place = autocomplete.getPlace();
+                $timeout(() => {
+                    ctrl.user.address.street = place.name;
+                    place.address_components.forEach((address_component) => {
+                        let index = address_component.types.indexOf('postal_code');
+                        if(index != -1){
+                            ctrl.user.address.zipCode = address_component.long_name;
+                        }
+                    });
+                })
+            });
         }
 
         ctrl.getFirstName = () => {
@@ -31,7 +54,7 @@ let RegisterComponent = {
         }
 
         ctrl.getOrganizationName = () => {
-            return (ctrl.user.organization||'').charAt(0).toUpperCase() + (ctrl.user.organization||'').slice(1);
+            return (ctrl.user.organization || '').charAt(0).toUpperCase() + (ctrl.user.organization || '').slice(1);
         }
 
         ctrl.isValidEmail = email => {
@@ -40,10 +63,10 @@ let RegisterComponent = {
 
         ctrl.setTypePerson = (type) => {
             ctrl.user.typePerson = type;
-            if(ctrl.user.typePerson == 'JURIDICA'){
+            if (ctrl.user.typePerson == 'JURIDICA') {
                 ctrl.continue();
             }
-            if(ctrl.user.typePerson == 'FISICA'){
+            if (ctrl.user.typePerson == 'FISICA') {
                 ctrl.step = 6;
             }
         }
@@ -53,17 +76,27 @@ let RegisterComponent = {
             ctrl.continue();
         }
 
+        ctrl.setFollow = follow => {
+            ctrl.user.follow = follow;
+            ctrl.continue();
+        }
+
         ctrl.isValidContinue = () => {
             let valid = true;
-            if(ctrl.step == 1){
-                if(!ctrl.user.name) valid = false;
-                if(!ctrl.user.email || !ctrl.isValidEmail(ctrl.user.email)) valid = false;
-                if(!ctrl.user.password || !ctrl.user.confirmPassword) valid = false;
-                if(ctrl.user.password != ctrl.user.confirmPassword) valid = false;
-                if((ctrl.user.password && ctrl.user.password.length < 3) || (ctrl.user.confirmPassword && ctrl.user.confirmPassword.length < 3)) valid = false;
+            if (ctrl.step == 1) {
+                if (!ctrl.user.name) valid = false;
+                if (!ctrl.user.email || !ctrl.isValidEmail(ctrl.user.email)) valid = false;
+                if (!ctrl.user.password || !ctrl.user.confirmPassword) valid = false;
+                if (ctrl.user.password != ctrl.user.confirmPassword) valid = false;
+                if ((ctrl.user.password && ctrl.user.password.length < 3) || (ctrl.user.confirmPassword && ctrl.user.confirmPassword.length < 3)) valid = false;
             }
-            if(ctrl.step == 2){
-                if(!ctrl.user.organization) valid = false;
+            if (ctrl.step == 2) {
+                if (!ctrl.user.organization) valid = false;
+            }
+            if (ctrl.step == 6) {
+                if (!ctrl.user.address.number) valid = false;
+                if (!ctrl.user.address.zipCode) valid = false;
+                if (!ctrl.user.address.street) valid = false;
             }
             return valid;
         };
@@ -76,7 +109,15 @@ let RegisterComponent = {
         };
 
         ctrl.continue = () => {
-            ctrl.step++;
+            ctrl.loading = true;
+            $timeout(() => {
+                delete ctrl.loading, 1500;
+                ctrl.step++;
+            }, 1500);
+        }
+
+        ctrl.back = () => {
+            ctrl.step--;
         }
 
     }]
