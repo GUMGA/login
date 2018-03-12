a15cb2a9b20fffc83896d946035a6c5d526b07b6
Mateus Miranda <info.mateusmiranda@gmail.com>
2018-03-12 10:09:38 -0300

    merge

--- src/components/login/component.js
+++ src/components/login/component.js
@@ -129,7 +129,7 @@ let LoginComponent = {
 
     </section>
   `,
-  controller: ['GumgaLoginService', '$timeout', '$scope', '$attrs', function(GumgaLoginService, $timeout, $scope, $attrs) {
+  controller: ['GumgaLoginService', '$timeout', '$scope', '$attrs', function (GumgaLoginService, $timeout, $scope, $attrs) {
     let ctrl = this;
     ctrl.loginText = 'Entrar';
     ctrl.changeOrganizationText = 'Continuar';
@@ -141,15 +141,15 @@ let LoginComponent = {
 
     const getUrlVars = () => {
       let vars = {}, parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi,
-      function(m,key,value) {
-        vars[key] = value;
-      });
+        function (m, key, value) {
+          vars[key] = value;
+        });
       return vars;
     }
 
     var params = getUrlVars();
 
-    if(ctrl.register){
+    if (ctrl.register) {
       ctrl.registerFields = ctrl.register.fields;
     }
     ctrl.organizationFilter = '';
@@ -163,60 +163,80 @@ let LoginComponent = {
     }, true);
 
     ctrl.$onInit = () => {
-      if(!ctrl.configuration){
+      if (!ctrl.configuration) {
         console.error("Please enter the setup object for the gumga login.");
       }
       GumgaLoginService.initConfiguration(ctrl.configuration);
+      if (params['ticket']) {
+        GumgaLoginService.searchTicket(params['ticket'])
+          .then(resp => {
+            if (resp.data.response == 'OK') {
+              ctrl.updateStep('NEW_PASSWORD');
+            } else if (resp.data.response == 'TOKEN_EXPIRED') {
+              ctrl.showMessage('error', 'Seu ticket de redefinição de senha foi expirado.', 3000, () => {
+                ctrl.removeParams();
+              });
+            } else {
+              ctrl.showMessage('error', 'Falha ao conectar com os servidores, aguarde...', 3000, () => {
+                ctrl.removeParams();
+              });
+            }
+          }, error => {
+            ctrl.showMessage('error', 'Falha ao conectar com os servidores, aguarde...', 3000, () => {
+              ctrl.removeParams();
+            });
+          })
+      }
     }
 
     ctrl.back = (step) => {
-        switch (step) {
-          case 'LOGIN':
-            ctrl.updateStep(step);
-            delete ctrl.password;
-            break;
-        }
+      switch (step) {
+        case 'LOGIN':
+          ctrl.updateStep(step);
+          delete ctrl.password;
+          break;
+      }
     }
 
     const createUserImage = (user) => {
-        if(user.picture){
-            user.profileImage = 'data:' + user.picture.mimeType + ';base64,' + user.picture.bytes;
-            return;
-        }
-        user.profileImage = 'src/images/user-without-image.png';
+      if (user.picture) {
+        user.profileImage = 'data:' + user.picture.mimeType + ';base64,' + user.picture.bytes;
+        return;
+      }
+      user.profileImage = 'src/images/user-without-image.png';
     }
 
     ctrl.submit = (email, password) => {
-      if(!email || !password){
+      if (!email || !password) {
         ctrl.showMessage('error', 'Precisamos que todas as informações sejam preenchidas.');
         return;
       }
-      if(!ctrl.isValidEmail(email)){
+      if (!ctrl.isValidEmail(email)) {
         ctrl.showMessage('error', 'Aparentemente você digitou um E-mail incorreto.');
         return;
       }
       ctrl.loginText = 'Aguarde...';
       GumgaLoginService.login(email, password)
-        .then(resp=>{
+        .then(resp => {
           ctrl.user = resp.data;
           createUserImage(ctrl.user);
           GumgaLoginService.setItemInSession('user', resp.data);
           GumgaLoginService.getOrganizations(resp.data.token)
             .then(organizations => {
-                GumgaLoginService.setItemInSession('organizations', organizations.data);
-                if(organizations.data.length == 1){
-                    ctrl.onLogin({user: resp.data, organizations: organizations.data});
-                }
-                if(organizations.data.length > 1){
-                    ctrl.updateStep('CHANGE_ORGANIZATION');
-                    ctrl.organizations = organizations.data;
-                }
+              GumgaLoginService.setItemInSession('organizations', organizations.data);
+              if (organizations.data.length == 1) {
+                ctrl.onLogin({ user: resp.data, organizations: organizations.data });
+              }
+              if (organizations.data.length > 1) {
+                ctrl.updateStep('CHANGE_ORGANIZATION');
+                ctrl.organizations = organizations.data;
+              }
             }, error => {
-                ctrl.onLogin({user: resp.data, organizations: undefined});
+              ctrl.onLogin({ user: resp.data, organizations: undefined });
             });
         }, error => {
-          if(error.data && error.data.response){
-            switch(error.data.response){
+          if (error.data && error.data.response) {
+            switch (error.data.response) {
               case 'NO_USER':
               case 'BAD_PASSWORD':
                 ctrl.showMessage('error', $attrs.textBadPassword || 'E-mail ou senha estão incorretos.');
@@ -240,30 +260,30 @@ let LoginComponent = {
     ctrl.changeOrganization = organization => {
       ctrl.changeOrganizationText = 'Aguarde...';
       GumgaLoginService.changeOrganization(ctrl.user.token, organization)
-        .then(resp=>{
-            sessionStorage.removeItem('user');
-            sessionStorage.removeItem('gumgaToken');
-            ctrl.user['token'] = resp.data['value'];
-            ctrl.user['organization'] = resp.data.organization['name'];
-            ctrl.user['organizationHierarchyCode'] = resp.data.organization['hierarchyCode'];
-            ctrl.user['softwareHouse'] = resp.data['isSoftwareHouse'];
-            ctrl.user['securityManager'] = resp.data['securityManager'];
-            sessionStorage.setItem('gumgaToken', resp.data['value']);
-            GumgaLoginService.setItemInSession('user', ctrl.user);
-            ctrl.loginText = 'Entrar';
-            ctrl.updateStep('LOGIN');
-            ctrl.onLogin({user: ctrl.user, organizations: ctrl.organizations});
-        }, error=> {
+        .then(resp => {
+          sessionStorage.removeItem('user');
+          sessionStorage.removeItem('gumgaToken');
+          ctrl.user['token'] = resp.data['value'];
+          ctrl.user['organization'] = resp.data.organization['name'];
+          ctrl.user['organizationHierarchyCode'] = resp.data.organization['hierarchyCode'];
+          ctrl.user['softwareHouse'] = resp.data['isSoftwareHouse'];
+          ctrl.user['securityManager'] = resp.data['securityManager'];
+          sessionStorage.setItem('gumgaToken', resp.data['value']);
+          GumgaLoginService.setItemInSession('user', ctrl.user);
+          ctrl.loginText = 'Entrar';
+          ctrl.updateStep('LOGIN');
+          ctrl.onLogin({ user: ctrl.user, organizations: ctrl.organizations });
+        }, error => {
           ctrl.changeOrganizationText = 'Erro :(';
         });
     }
 
     ctrl.isUrl = url => {
-        return /(http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/.test(url);
+      return /(http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/.test(url);
     }
 
     ctrl.isValidEmail = email => {
-        return /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+      return /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
     }
 
     const getRegisterFieldByName = name => {
@@ -271,26 +291,26 @@ let LoginComponent = {
     }
 
     const handlingErrorRegister = (field, comparatorField, valid) => {
-        var inputComparator = document.getElementById(comparatorField.field)
-        var input = document.getElementById(field.field)
-        if(inputComparator && input){
-          inputComparator.classList[valid?'remove':'add']('gumga-login-input-error');
-          input.classList[valid?'remove':'add']('gumga-login-input-error');
-        }
+      var inputComparator = document.getElementById(comparatorField.field)
+      var input = document.getElementById(field.field)
+      if (inputComparator && input) {
+        inputComparator.classList[valid ? 'remove' : 'add']('gumga-login-input-error');
+        input.classList[valid ? 'remove' : 'add']('gumga-login-input-error');
+      }
     }
 
     ctrl.registerIsValid = () => {
       let valid = true;
-      if(!ctrl.registerFields){return}
+      if (!ctrl.registerFields) { return }
       ctrl.registerFields.forEach(field => {
-        if(field.conditions){
-          field.conditions.forEach(condition=>{
-            if(condition.operation && condition.field){
-               var comparatorField = getRegisterFieldByName(condition.field)
-               if(valid){
-                  valid = eval("'"+field.value+"' "+condition.operation+" '"+comparatorField.value+"'");
-                  handlingErrorRegister(field, comparatorField, valid);
-               }
+        if (field.conditions) {
+          field.conditions.forEach(condition => {
+            if (condition.operation && condition.field) {
+              var comparatorField = getRegisterFieldByName(condition.field)
+              if (valid) {
+                valid = eval("'" + field.value + "' " + condition.operation + " '" + comparatorField.value + "'");
+                handlingErrorRegister(field, comparatorField, valid);
+              }
             }
           })
         }
@@ -300,7 +320,7 @@ let LoginComponent = {
 
     ctrl.submitRegister = () => {
       ctrl.registerText = 'Aguarde...';
-      if(!ctrl.registerFields){return}
+      if (!ctrl.registerFields) { return }
       var toReturn = {};
       ctrl.registerFields.forEach(field => {
         toReturn[field.field] = field.value;
@@ -314,44 +334,44 @@ let LoginComponent = {
       ctrl.forgotButtonText = 'Aguarde...';
       ctrl.disabledForgotButton = true;
       GumgaLoginService.lostPassword(email)
-      .then(resp => {
-        if(resp.data.response == 'OK'){
-          ctrl.showMessage('success', 'Solicitação de nova senha enviada, verifique seu E-mail.');
-          delete ctrl.forgotEmail;
+        .then(resp => {
+          if (resp.data.response == 'OK') {
+            ctrl.showMessage('success', 'Solicitação de nova senha enviada, verifique seu E-mail.');
+            delete ctrl.forgotEmail;
+            ctrl.updateStep('LOGIN');
+          } else if (resp.data.response == 'NO_USER') {
+            ctrl.showMessage('error', 'O E-mail informado não possui registro.');
+          }
+          ctrl.forgotButtonText = 'Redefinir';
+          ctrl.disabledForgotButton = false;
+        }, error => {
+          ctrl.showMessage('error', 'Falha ao conectar com os servidores, tente mais tarde.');
+          ctrl.forgotButtonText = 'Redefinir';
+          ctrl.disabledForgotButton = false;
           ctrl.updateStep('LOGIN');
-        }else if(resp.data.response == 'NO_USER'){
-          ctrl.showMessage('error', 'O E-mail informado não possui registro.');
-        }
-        ctrl.forgotButtonText = 'Redefinir';
-        ctrl.disabledForgotButton = false;
-      }, error => {
-        ctrl.showMessage('error', 'Falha ao conectar com os servidores, tente mais tarde.');
-        ctrl.forgotButtonText = 'Redefinir';
-        ctrl.disabledForgotButton = false;
-        ctrl.updateStep('LOGIN');
-      })
+        })
     }
 
     ctrl.updatePassword = (lost) => {
       ctrl.lostButtonText = 'Aguarde...';
       ctrl.disabledLostButton = true;
-      if(!lost.password || (lost.password != lost.confirmPassword)){
-          ctrl.showMessage('error', 'Verifique se você repetiu sua senha corretamente.');
-          return;
+      if (!lost.password || (lost.password != lost.confirmPassword)) {
+        ctrl.showMessage('error', 'Verifique se você repetiu sua senha corretamente.');
+        return;
       }
 
       GumgaLoginService.updatePassword(params['ticket'], lost.password)
         .then(resp => {
-          if(resp.data.response == 'OK'){
+          if (resp.data.response == 'OK') {
             ctrl.showMessage('success', 'Sua senha foi redefinida com sucesso.');
             ctrl.lostButtonText = 'Redefinir';
             ctrl.disabledLostButton = false;
             ctrl.updateStep('LOGIN');
-          }else if(resp.data.response == 'TOKEN_EXPIRED'){
-              ctrl.showMessage('error', 'Seu ticket de redefinição de senha foi expirado.', 3000, () => {
-                ctrl.removeParams();
-              });
-          }else{
+          } else if (resp.data.response == 'TOKEN_EXPIRED') {
+            ctrl.showMessage('error', 'Seu ticket de redefinição de senha foi expirado.', 3000, () => {
+              ctrl.removeParams();
+            });
+          } else {
             ctrl.showMessage('error', 'Falha ao conectar com os servidores, aguarde...', 3000, () => {
               ctrl.removeParams();
             });
@@ -364,43 +384,22 @@ let LoginComponent = {
     }
 
     ctrl.showMessage = (type, message, time, callback) => {
-        ctrl.alertErrorMessage = {
-          message: message,
-          type: type
-        };
-        $timeout(() => {
-          delete ctrl.alertErrorMessage
-          if(callback) callback();
-        }, (time || 3000));
+      ctrl.alertErrorMessage = {
+        message: message,
+        type: type
+      };
+      $timeout(() => {
+        delete ctrl.alertErrorMessage
+        if (callback) callback();
+      }, (time || 3000));
     }
 
     ctrl.removeParams = () => {
       location.href = location.href.substring(0, location.href.lastIndexOf('?'));
     }
 
-    if(params['email']){
+    if (params['email']) {
       ctrl.registerSaaS = true;
-    }
-
-    if(params['ticket']){
-       GumgaLoginService.searchTicket(params['ticket'])
-        .then(resp => {
-          if(resp.data.response == 'OK'){
-            ctrl.updateStep('NEW_PASSWORD');
-          }else if(resp.data.response == 'TOKEN_EXPIRED'){
-              ctrl.showMessage('error', 'Seu ticket de redefinição de senha foi expirado.', 3000, () => {
-                ctrl.removeParams();
-              });
-          }else{
-            ctrl.showMessage('error', 'Falha ao conectar com os servidores, aguarde...', 3000, () => {
-              ctrl.removeParams();
-            });
-          }
-        }, error => {
-          ctrl.showMessage('error', 'Falha ao conectar com os servidores, aguarde...', 3000, () => {
-            ctrl.removeParams();
-          });
-        })
     }
 
   }]
